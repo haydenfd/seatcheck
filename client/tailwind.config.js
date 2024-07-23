c90fa5192ee5e5f4db5d031a949a5920b49721d1
Hayden D’Souza <haydenfds@gmail.com>
2024-07-23 01:04:53 -0700

    Added initial shadcn components + fixed tailwind config to accept both nextui and shadcn

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,23 +1,48 @@
-const { nextui } = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
+import commonjs from "vite-plugin-commonjs";
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       colors: {
         "ucla-gold": "#FFD100",
         "ucla-blue": "#2774AE",
       },
       boxShadow: {
         subtle: "0 4px 12px rgba(255, 179, 0, 0.5)",
-      },
+      },      
     },
   },
-  darkMode: "class",
-  plugins: [nextui()]
+  plugins: [require("tailwindcss-animate"), nextui(), commonjs()],
 }
